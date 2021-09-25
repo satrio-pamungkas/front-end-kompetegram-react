7aa7eebb860037023b691424b43376fd2611b031
aderio25 <adewipo@gmail.com>
2021-09-25 17:37:33 +0700

    feat: added navbar design

--- src/components/Beranda/Navbar.jsx
+++ src/components/Beranda/Navbar.jsx
@@ -2,28 +2,26 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
+import Logo from "../../assets/img/LogoKTGHorizontal.svg";
 
 export const NavigationBar = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container className="navbar-beranda">
-                <Navbar.Brand href="#home">
-                    <Image src="../assets/img/LogoKTGHorizontal.svg" fluid/>
+                <Navbar.Brand href="">
+                    <Image className="logo" src={Logo} fluid/>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">BERANDA</Nav.Link>
-                        <Nav.Link href="#pricing">TENTANG</Nav.Link>
-                        <Nav.Link href="#pricing">PENGURUS</Nav.Link>
-                        <Nav.Link href="#pricing">KEGIATAN</Nav.Link>
-                        <Nav.Link href="#pricing">KONTAK</Nav.Link>
+                        <Nav.Link href="">BERANDA</Nav.Link>
+                        <Nav.Link href="">TENTANG</Nav.Link>
+                        <Nav.Link href="">KEGIATAN</Nav.Link>
+                        <Nav.Link href="">KONTAK</Nav.Link>
                     </Nav>
-                    <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
+                    <Nav className="masuk-daftar">
+                        <Nav.Link href="">MASUK</Nav.Link>
+                        <Nav.Link className="button-daftar" href="">DAFTAR</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
